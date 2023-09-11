55613e8749454362357be39a1e8c686b1542ab03
Hidetaka <quyhoang@users.noreply.github.com>
2023-09-11 13:26:32 +0900

    Create segmentPhase.m

---
+++ segmentPhase.m
@@ -0,0 +1,5 @@
+function segmentPhase = segmentPhase(P1,P2)
+% return the angle of the vector from point P1 to point P2
+complexNum = P2(1) - P1(1) + (P2(2) - P1(2))*1i;
+segmentPhase = rad2deg(angle(complexNum));
+end
